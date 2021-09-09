f8bf4b2da4cc479d353d2624a6dac59a5f7f0c37
tc-jscheid <jscheid@tigerconnect.com>
2021-09-09 13:45:01 -0700

    more formatting fixes

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 def main():
-    #my_input = os.environ["tfsec.sarif"]
+
+
     file_path = os.environ["INPUT_PATH"]
 
-    with open(os.environ["INPUT_PATH"]) as f:
+    with open(file_path) as f:
         data = json.load(f)
 
         rules = data['runs'][0]['tool']['driver']['rules']
