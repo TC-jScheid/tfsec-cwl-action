2b82e25f96872fca929ab8641d455dd0fca48178
tc-jscheid <jscheid@tigerconnect.com>
2021-09-09 15:17:36 -0700

    read aws key

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import boto3
 
 def main():
 
-    AWS_KEY_ID = os.environ["AWS_KEY_ID_GITHUB_ACTIONS"]
-    AWS_KEY_SECRET = os.environ["AWS_KEY_SECRET_GITHUB_ACTIONS"]
+    AWS_KEY_ID = os.environ["INPUT_AWS_KEY_ID"]
+    AWS_KEY_SECRET = os.environ["INPUT_AWS_KEY_SECRET"]
     file_path = os.environ["INPUT_REPORT"]
 
     with open(file_path) as f:
