ef0b598ee947bf35322195341e68fb751c5171ef
tc-jscheid <jscheid@tigerconnect.com>
2021-09-09 13:43:16 -0700

    cleaned up from linter

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ import os
 import json
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 
-
 def main():
     #my_input = os.environ["tfsec.sarif"]
     file_path = os.environ["INPUT_PATH"]
-    
+
     with open(os.environ["INPUT_PATH"]) as f:
         data = json.load(f)
 
         rules = data['runs'][0]['tool']['driver']['rules']
 
         for r in rules:
-            print(f"Rule: {r} ")    
-    
-
+            print(f"Rule: {r} ")
 
 if __name__ == "__main__":
     main()
