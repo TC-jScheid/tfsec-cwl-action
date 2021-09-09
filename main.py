7190d657778ef18582399fe12bd869056118fe4b
tc-jscheid <jscheid@tigerconnect.com>
2021-09-09 16:08:35 -0700

    adding code to push to cwl

--- main.py
+++ main.py
@@ -3,21 +3,48 @@ import json
 import boto3
 
 def main():
-
+    # Input Variables
+    # AWS Credentials
     AWS_KEY_ID = os.environ["INPUT_AWS_KEY_ID"]
     AWS_KEY_SECRET = os.environ["INPUT_AWS_KEY_SECRET"]
+    #File path of tfsec report
     file_path = os.environ["INPUT_REPORT"]
-
+    # CWL Group
+    cwl_group = os.environ["INPUT_CWL_GROUP"]
+    # CWL Stream
+    cwl_stream = os.environ["INPUT_CWL_STREAM"]
+    #Initialize rules list
+    rules = []
+    #Instantiate client with CWL
+    client = boto3.client('logs')
+    
     with open(file_path) as f:
         data = json.load(f)
-
+        # CAREFULLY DECONSTRUCTED from serif format
         rules = data['runs'][0]['tool']['driver']['rules']
 
-# Send to cloudwatch logs as 1 json object
-        print(f"Sending rules using key: {AWS_KEY_ID}")
+    #Check if CWL stream is created
+    response = client.create_log_stream(
+        logGroupName=cwl_group,
+        logStreamName=cwl_stream
+    )
 
-        for r in rules:
-            print(f"Rule: {r} ")
+    if not response:
+        putResponse = client.put_log_events(
+            logGroupName=cwl_group,
+            logStreamName=cwl_stream,
+            # Include repo, and branch, and user
+            logEvents=[
+                {
+                    'Repository': 123,
+                    'Branch': 'branch1',
+                    'User': 'user1',
+                    'Rules': rules
+                },
+            ]
+        )
+
+    
 
 if __name__ == "__main__":
     main()
